d4b09fe832c7453439429f8351fa8f6264244bd3
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-07-31 16:15:25 +0530

    Made Visual Changes

--- cash-register-manager/src/App.js
+++ cash-register-manager/src/App.js
@@ -1,17 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
+  function checkChange() {}
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cash Register Manager</h1>
+        <div className="head__div">
+          <h1>Cash Register Manager</h1>
+          <img className="App-logo" src={logo} alt="" />
+        </div>
         <p>
           Enter the bill amount and cash given by the customer and know minimum
           number of notes to return.
         </p>
       </header>
-      <main></main>
+      <main className="main">
+        <div className="main__div">
+          <p>Bill Amount: </p>
+          <input />
+          <p>Cash Given: </p>
+          <input />
+          <br />
+          <br />
+          <button>Check</button>
+        </div>
+      </main>
     </div>
   );
 }
