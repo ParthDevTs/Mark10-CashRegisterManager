f9811743ffba8b06bc1f03e5c3a1755374c913a5
Parth Kathuria <88901443+ParthDevTs@users.noreply.github.com>
2022-07-31 16:36:54 +0530

    added buttons and logic

--- cash-register-manager/src/App.js
+++ cash-register-manager/src/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
 import logo from "./logo.svg";
+import { useState } from "react";
 
 function App() {
-  function checkChange() {}
+  var [cashPaid, setCashPaid] = useState(0);
+  var [billAmount, setBillAmount] = useState(0);
+  var [outputStatement, setOutputStatement] = useState("");
+
+  function checkChange() {
+    var cashremaining = cashPaid - billAmount;
+    if (cashremaining < 0) {
+      setOutputStatement("You can wash Dishes to pay the rest");
+    }
+  }
+
+  function billamount(event) {
+    var value = event.target.value;
+    setBillAmount(value);
+  }
+  function cashGiven(event) {
+    var value = event.target.value;
+    setCashPaid(value);
+  }
 
   return (
     <div className="App">
@@ -19,12 +38,13 @@ function App() {
       <main className="main">
         <div className="main__div">
           <p>Bill Amount: </p>
-          <input />
+          <input type="number" className="bill-amt" onChange={billamount} />
           <p>Cash Given: </p>
-          <input />
+          <input type="number" className="cash-given" onChange={cashGiven} />
           <br />
           <br />
-          <button>Check</button>
+          <button onClick={checkChange}>Check</button>
+          <p className="output-statement">{outputStatement}</p>
         </div>
       </main>
     </div>
